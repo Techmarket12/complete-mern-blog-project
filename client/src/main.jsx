11cbb61493576904beef479997675711c3c5a47b
= <aymazhamza@gmail.com>
2025-03-19 07:56:45 +0100

    second commit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,8 @@ import { Provider } from 'react-redux'
 import {PersistGate} from 'redux-persist/integration/react'
 import ThemeProvider from './components/ThemeProvider.jsx'
 
+
+
 createRoot(document.getElementById('root')).render(
   <PersistGate persistor={persistor}>
 
